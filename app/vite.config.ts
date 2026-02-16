47257b94e4b301a973867222d1855a1358e041c8
Amr Abdelhalim <amrgomaa2002@outlook.com>
2026-02-16 19:02:11 +0200

    Project Dev Finished

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,21 +2,26 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/mobile-social-e1/' : '/',
-  plugins: [
-    react(),
-    legacy()
-  ],
-  define: {
-    global: 'window',
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const baseUrl = env.VITE_BASE_URL || (process.env.GITHUB_ACTIONS ? '/mobile-recipes-e1/' : '/')
+
+  return {
+    base: baseUrl,
+    plugins: [
+      react(),
+      legacy()
+    ],
+    define: {
+      global: 'window',
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+    }
   }
 })
